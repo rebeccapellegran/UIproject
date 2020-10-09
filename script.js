573294aafcd40ffb8655700881254a31a4a9304b
Rebecca Pellegran <rebeccapellegran@gmail.com>
2020-10-09 12:34:02 -0400

    save website

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
-// const url = 'https://icanhazdadjoke.com/api'
+ const url = 'https://icanhazdadjoke.com/api'
 
 // function getDadJokes
 // .then(resp => resp.json())
 // console.log(resp)
+var slideIndex = 1;
+showSlides(slideIndex);
+function changeSlides(n) {
+   showSlides((slideIndex += n));
+}
+function currentSlide(n) {
+   showSlides((slideIndex = n));
+}
+function showSlides(n) {
+   var i;
+   var slides = document.getElementsByClassName("Slide");
+   var dots = document.getElementsByClassName("Navdot");
+   if (n > slides.length) {
+      slideIndex = 1;
+   }
+   if (n < 1) {
+      slideIndex = slides.length;
+   }
+   Array.from(slides).forEach(item => (item.style.display = "none"));
+   Array.from(dots).forEach(
+      item => (item.className = item.className.replace(" selected", ""))
+   );
+   slides[slideIndex - 1].style.display = "block";
+   dots[slideIndex - 1].className += " selected";
+}
